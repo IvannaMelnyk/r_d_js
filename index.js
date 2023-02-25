827d941a2c4fe89174a69239fbf34c23288bfa45
Ivanna Melnyk <ivanna.melnyk@freshbooks.com>
2023-02-25 13:32:44 +0200

    Total sum prompt

---
+++ index.js
@@ -0,0 +1,8 @@
+let firstNumber;
+let secondNumber;
+
+firstNumber = prompt("Enter first number");
+secondNumber = prompt("Enter second number");
+const total = Number(firstNumber) + Number(secondNumber);
+
+alert(`Total: ${total}`);
