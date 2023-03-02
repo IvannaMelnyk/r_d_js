20c7326834e4db66296bbf1e2b967ec4a3607917
Ivanna Melnyk <ivanna.melnyk@freshbooks.com>
2023-03-02 20:58:54 +0200

    else if condition

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ let secondNumber;
 
 firstNumber = prompt("Enter first number");
 secondNumber = prompt("Enter second number");
-const total = Number(firstNumber) + Number(secondNumber);
+const totalSum = Number(firstNumber) + Number(secondNumber);
+const totalSubstraction = Number(firstNumber) - Number(secondNumber);
+const totalMultiplication = Number(firstNumber) * Number(secondNumber);
+const totalDeviding = Number(firstNumber) / Number(secondNumber);
 
-alert(`Total: ${total}`);
+if (firstNumber === "" || secondNumber === "")
+  alert("Wrong operation. Write a number");
+else if (firstNumber < secondNumber) {
+  alert(`Addition amount: ${totalSum}`);
+  confirm("Are you sure that you want to continue?");
+  alert(`Substraction amount: ${totalSubstraction}`);
+  alert(`Multiplication amount: ${totalMultiplication}`);
+  alert(`Deviding amount: ${totalDeviding}`);
+} else if (secondNumber == 0) {
+  alert(`Addition amount: ${totalSum}`);
+  alert(`Substraction amount: ${totalSubstraction}`);
+  alert(`Multiplication amount: ${totalMultiplication}`);
+  alert(`Deviding amount: wrong value`);
+} else {
+  alert(`Addition amount: ${totalSum}`);
+  alert(`Substraction amount: ${totalSubstraction}`);
+  alert(`Multiplication amount: ${totalMultiplication}`);
+  alert(`Deviding amount: ${totalDeviding}`);
+}
