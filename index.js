4622fbed2ff10226390afeadcef2ddf459f34e46
Ivanna Melnyk <ivanna.melnyk@freshbooks.com>
2023-03-02 19:06:50 +0200

    Added alerts messages

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ let secondNumber;
 
 firstNumber = prompt("Enter first number");
 secondNumber = prompt("Enter second number");
-const total = Number(firstNumber) + Number(secondNumber);
+const totalSum = Number(firstNumber) + Number(secondNumber);
+const totalSubstraction = Number(firstNumber) - Number(secondNumber);
+const totalMultiplication = Number(firstNumber) * Number(secondNumber);
+const totalDeviding = Number(firstNumber) / Number(secondNumber);
 
-alert(`Total: ${total}`);
+alert(`Total addition amount: ${totalSum}`);
+alert(`Total substraction amount: ${totalSubstraction}`);
+alert(`Total multiplication amount: ${totalMultiplication}`);
+alert(`Total Deviding amount: ${totalDeviding}`);
